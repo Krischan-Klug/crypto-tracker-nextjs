30f274794e2f75d5381fdfb50c9b92ae1085ca65
Krischan-Klug <krischanklug@icloud.com>
2024-03-14 17:43:34 +0100

    add fetch

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import { SWRConfig } from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{
+        fetcher,
+        refreshInterval: 5000,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 export default function Home() {
   const router = useRouter();
+
+  const { data, error, isLoading } = useSWR(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
   return (
     <>
-      <h1>Welcome to crypto</h1>
+      <h1>KKs Cryptotracker</h1>
       <button onClick={() => router.push("/favorites")}>Favorites</button>
     </>
   );
